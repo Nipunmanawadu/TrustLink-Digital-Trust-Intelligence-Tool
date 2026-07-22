b759ebfae6c5265349a4252d4637a4d5a490bd07
Nipun <manawadunipun0@gmail.com>
2026-07-22 18:56:47 +0530

    Updated Intialized

---
+++ src/main/java/model/Risk.java
@@ -0,0 +1,34 @@
+package model;
+
+public class Risk {
+
+    private String name;
+    private int score;
+
+    public Risk(String name, int score) {
+
+        this.name = name;
+        this.score = score;
+
+    }
+
+    public String getName() {
+
+        return name;
+
+    }
+
+    public int getScore() {
+
+        return score;
+
+    }
+
+    @Override
+    public String toString() {
+
+        return name + " : " + score;
+
+    }
+
+}
