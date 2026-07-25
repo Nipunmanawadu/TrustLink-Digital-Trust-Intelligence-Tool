614bcc669c418c958580bab5e475d036248bc085
bornloardzzz <petsforboredom@gmail.com>
2026-07-25 10:27:30 +0530

    Created BFS class

---
+++ src/main/java/algorithm/BFS.java
@@ -0,0 +1,40 @@
+Package algorithm;
+
+import datastructure.Node;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BFS {
+
+    public void traverse(Node startNode) {
+
+        ArrayList<Node> visited = new ArrayList<>();
+
+        Queue<Node> queue = new LinkedList<>();
+
+        queue.add(startNode);
+        visited.add(startNode);
+
+        while (!queue.isEmpty()) {
+
+            Node current = queue.poll();
+
+            System.out.println(current);
+
+            for (Node neighbour : current.getConnections()) {
+
+                if (!visited.contains(neighbour)) {
+
+                    visited.add(neighbour);
+                    queue.add(neighbour);
+
+                }
+
+            }
+
+        }
+
+    }
+
+}
